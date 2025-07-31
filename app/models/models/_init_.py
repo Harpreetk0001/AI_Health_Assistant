e915704af37cdc0bafe93123109192dcdfeff3ff
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-07-31 15:09:58 +1000

    Update _init_.py

--- app/models/models/_init_.py
+++ app/models/models/_init_.py
@@ -1,1 +1,8 @@
+from .user import User, UIPreference
+from .health import HealthVital, DeviceIntegration, Suggestion, MentalHealthLog
+from .fall_event import FallEvent
+from .emergency_contact import EmergencyContact
+from .medication import MedicationSchedule, ReminderLog
+from .conversation import ConversationLog
+from .activity import ActivityLog
 
