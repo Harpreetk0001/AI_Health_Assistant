e65bd57c37e14857644eeb40ce83ed183bc33d27
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:07:05 +1000

    Update device_integration.py

--- app/api/endpoints/device_integration.py
+++ app/api/endpoints/device_integration.py
@@ -1,1 +1,9 @@
+router = APIRouter()
 
+@router.get("/", response_model=List[str])
+def read_device_integrations(db: Session = Depends(get_db)):
+    return ["Sample device integration"]
+
+@router.post("/", response_model=str)
+def create_device_integration(db: Session = Depends(get_db)):
+    return "Created device integration"
