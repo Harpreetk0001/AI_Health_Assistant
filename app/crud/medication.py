3d2381d96e4291014fcf243531043206b5ae011b
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:17:18 +1000

    Update medication.py

--- app/crud/medication.py
+++ app/crud/medication.py
@@ -1,1 +1,11 @@
+from app.models import Medication
+from app.db.session import db
 
+def create_medication(medication):
+    db.add(medication)
+    db.commit()
+    db.refresh(medication)
+    return medication
+
+def get_medication_by_id(medication_id):
+    return db.query(Medication).filter(Medication.id == medication_id).first()
