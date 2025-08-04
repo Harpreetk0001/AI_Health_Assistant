043a2183eb257513a9422103b54df6e7219b6856
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:16:11 +1000

    Update device_integration.py

--- app/crud/device_integration.py
+++ app/crud/device_integration.py
@@ -1,1 +1,11 @@
+from app.models import DeviceIntegration
+from app.db.session import db
 
+def create_device_integration(device_integration):
+    db.add(device_integration)
+    db.commit()
+    db.refresh(device_integration)
+    return device_integration
+
+def get_device_integration_by_id(device_integration_id):
+    return db.query(DeviceIntegration).filter(DeviceIntegration.id == device_integration_id).first()
