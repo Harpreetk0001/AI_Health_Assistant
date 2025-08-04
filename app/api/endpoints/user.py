59490a54d213845766ecdfc259985e0adeeab6a3
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:10:42 +1000

    Update user.py

--- app/api/endpoints/user.py
+++ app/api/endpoints/user.py
@@ -1,1 +1,9 @@
+router = APIRouter()
 
+@router.get("/", response_model=List[str])
+def read_users(db: Session = Depends(get_db)):
+    return ["Sample user"]
+
+@router.post("/", response_model=str)
+def create_user(db: Session = Depends(get_db)):
+    return "Created user"
