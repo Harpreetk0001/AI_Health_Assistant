bdad7df6f5b5a1d6b542bcb4d0a18501df7c1419
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:24:37 +1000

    Update suggestion.py

--- app/models/models/suggestion.py
+++ app/models/models/suggestion.py
@@ -1,1 +1,9 @@
+from sqlalchemy import Column, String, DateTime, ForeignKey, Text
+from app.db.base_class import Base
 
+class Suggestion(Base):
+    __tablename__ = "suggestions"
+    id = Column(String, primary_key=True, index=True)
+    user_id = Column(String, ForeignKey("users.id"))
+    suggestion_text = Column(Text, nullable=False)
+    created_at = Column(DateTime, nullable=False)
