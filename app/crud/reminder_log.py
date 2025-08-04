4d4d5c7f18337aba73eb53eea33168f1a0aef51f
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:17:36 +1000

    Update reminder_log.py

--- app/crud/reminder_log.py
+++ app/crud/reminder_log.py
@@ -1,1 +1,11 @@
+from app.models import ReminderLog
+from app.db.session import db
 
+def create_reminder_log(reminder):
+    db.add(reminder)
+    db.commit()
+    db.refresh(reminder)
+    return reminder
+
+def get_reminder_log_by_id(reminder_id):
+    return db.query(ReminderLog).filter(ReminderLog.id == reminder_id).first()
