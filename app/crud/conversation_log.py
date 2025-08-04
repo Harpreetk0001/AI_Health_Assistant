6a50eb6cd3978f8942374fa1c2962e98bdf06232
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:15:56 +1000

    Update conversation_log.py

--- app/crud/conversation_log.py
+++ app/crud/conversation_log.py
@@ -1,1 +1,11 @@
+from app.models import ConversationLog
+from app.db.session import db
 
+def create_conversation_log(conversation_log):
+    db.add(conversation_log)
+    db.commit()
+    db.refresh(conversation_log)
+    return conversation_log
+
+def get_conversation_log_by_id(conversation_log_id):
+    return db.query(ConversationLog).filter(ConversationLog.id == conversation_log_id).first()
