f7cd8f98f4c6e1bf233df67d076c98a8ee7def84
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:33:02 +1000

    Update emergency_contact.py

--- app/schemas/emergency_contact.py
+++ app/schemas/emergency_contact.py
@@ -1,1 +1,10 @@
+from sqlalchemy import Column, String, ForeignKey
+from app.db.base_class import Base
 
+class EmergencyContact(Base):
+    __tablename__ = "emergency_contacts"
+    id = Column(String, primary_key=True, index=True)
+    user_id = Column(String, ForeignKey("users.id"))
+    name = Column(String, nullable=False)
+    phone = Column(String, nullable=False)
+    relationship = Column(String)
