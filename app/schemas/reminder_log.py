470a215758d9dd0dec7fb63921be6275c5acc20c
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:39:11 +1000

    Update reminder_log.py

--- app/schemas/reminder_log.py
+++ app/schemas/reminder_log.py
@@ -1,1 +1,23 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class ReminderLogBase(BaseModel):
+    message: str
+    remind_at: datetime
+    completed: bool | None = False
+
+class ReminderLogCreate(ReminderLogBase):
+    user_id: str
+
+class ReminderLogUpdate(BaseModel):
+    message: str | None = None
+    remind_at: datetime | None = None
+    completed: bool | None = None
+
+class ReminderLog(ReminderLogBase):
+    id: str
+    user_id: str
+
+    class Config:
+        orm_mode = True
 
