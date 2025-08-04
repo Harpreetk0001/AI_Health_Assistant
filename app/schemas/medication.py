f50e579f76b10b7b5cdbb40107df4b3eb5d1492c
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:37:35 +1000

    Update medication.py

--- app/schemas/medication.py
+++ app/schemas/medication.py
@@ -1,1 +1,25 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class MedicationBase(BaseModel):
+    name: str
+    dosage: str | None = None
+    start_date: datetime | None = None
+    end_date: datetime | None = None
+
+class MedicationCreate(MedicationBase):
+    user_id: str
+
+class MedicationUpdate(BaseModel):
+    name: str | None = None
+    dosage: str | None = None
+    start_date: datetime | None = None
+    end_date: datetime | None = None
+
+class Medication(MedicationBase):
+    id: str
+    user_id: str
+
+    class Config:
+        orm_mode = True
 
