d2663f5cd924dc44bd1f3199ebc905ea1cde7323
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:39:46 +1000

    Update suggestion.py

--- app/schemas/suggestion.py
+++ app/schemas/suggestion.py
@@ -1,1 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class SuggestionBase(BaseModel):
+    suggestion_text: str
+
+class SuggestionCreate(SuggestionBase):
+    user_id: str
+    created_at: datetime
+
+class SuggestionUpdate(BaseModel):
+    suggestion_text: str | None = None
+
+class Suggestion(SuggestionBase):
+    id: str
+    user_id: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
 
