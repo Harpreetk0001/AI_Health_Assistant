7be940185a2d703679014ae1e46d0359028776b0
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-04 16:15:39 +1000

    Update activity_log.py

--- app/crud/activity_log.py
+++ app/crud/activity_log.py
@@ -1,1 +1,11 @@
+from app.models import ActivityLog
+from app.db.session import db
 
+def create_activity_log(activity_log):
+    db.add(activity_log)
+    db.commit()
+    db.refresh(activity_log)
+    return activity_log
+
+def get_activity_log_by_id(activity_log_id):
+    return db.query(ActivityLog).filter(ActivityLog.id == activity_log_id).first()
