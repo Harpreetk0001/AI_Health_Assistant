e8f1b1d66f820add5f4998f80cbf1cf8c51b01cc
mehb0-0 <mehakb.2318@gmail.com>
2025-08-05 16:25:27 +1000

    Add files via upload

---
+++ HealthMonitoringCore.py
@@ -0,0 +1,15 @@
+import random
+import json
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+#grap current vitals via BlueTooth API#
+
+#health monitoring grab historical vital signs from JSON DB#
+
+#health monitoring graph visualisation#
+
+#health monitoring alerts#
+
+#health monitoring AI integration#
