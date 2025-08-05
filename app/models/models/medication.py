36f222c8e34887612da84baa980d7f50178176f6
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-06 00:08:56 +1000

    Update medication.py

--- app/models/models/medication.py
+++ app/models/models/medication.py
@@ -1,11 +1,15 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey
-from app.db.base_class import Base
+from sqlalchemy import Column, String, DateTime, UUID, ForeignKey
+from db.base import Base
+import uuid
 
 class Medication(Base):
-    __tablename__ = "medications"
-    id = Column(String, primary_key=True, index=True)
-    user_id = Column(String, ForeignKey("users.id"))
+    __tablename__ = "medication"
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
     name = Column(String, nullable=False)
-    dosage = Column(String)
-    start_date = Column(DateTime)
-    end_date = Column(DateTime)
+    dosage = Column(String, nullable=False)
+    frequency = Column(String, nullable=False)
+    start_date = Column(DateTime, nullable=False)
+    end_date = Column(DateTime, nullable=True)
+    instructions = Column(String, nullable=True)
