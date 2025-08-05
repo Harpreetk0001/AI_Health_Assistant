86531e0b9584b2b8a6eb94070692ea8bf217b782
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-06 00:10:08 +1000

    Update conversation.py

--- app/models/models/conversation.py
+++ app/models/models/conversation.py
@@ -1,10 +1,12 @@
-from sqlalchemy import Column, String, DateTime, ForeignKey
-from app.db.base_class import Base
+from sqlalchemy import Column, String, DateTime, UUID, ForeignKey
+from db.base import Base
+import uuid
 
-class Conversation(Base):
+class ConversationLog(Base):
     __tablename__ = "conversation_logs"
-    id = Column(String, primary_key=True, index=True)
-    user_id = Column(String, ForeignKey("users.id"))
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    user_id = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    sender = Column(String, nullable=False)  # "user" or "assistant"
     message = Column(String, nullable=False)
-    sender = Column(String, nullable=False)  # user or assistant
     timestamp = Column(DateTime, nullable=False)
