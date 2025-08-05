6eae06f8a5d4a822713ad5e19200b3ef58faf52a
mehb0-0 <mehakb.2318@gmail.com>
2025-08-05 15:48:09 +1000

    Create main.py
    main functionality and integration where API calls, data processing, integration takes place i.e. system flowchart in code

---
+++ main.py
@@ -0,0 +1,18 @@
+##MAIN##
+
+#import files for front-end, backend, dev, ai models
+
+#load files and data using APIs
+
+#define variables with data
+
+#call functions on variables#
+
+#if user data/information records are empty
+    #prompt login page/profile page
+    #display message prompting them to fill in information
+
+#else if user information is there
+    #display home page from front end 
+        #load data from backend to front end
+        #load core functionality
