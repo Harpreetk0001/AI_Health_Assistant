db23c092918eb4c93f33c5d233a44f187eaaa442
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-05 17:48:47 +1000

    Update health.py

--- app/api/endpoints/health.py
+++ app/api/endpoints/health.py
@@ -1,16 +1,26 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app.db.session import get_db
-from app.schemas.health import HealthRecord, HealthRecordCreate
-from app.crud import health as crud_health
-from typing import List
+from app import crud, schemas
+from app.db.database import get_db
 
 router = APIRouter()
 
-@router.post("/health_records/", response_model=HealthRecord)
-def create_record(record: HealthRecordCreate, db: Session = Depends(get_db)):
-    return crud_health.create_health_record(db=db, health=record)
+@router.post("/", response_model=schemas.Health)
+def create_health_record(health: schemas.HealthCreate, db: Session = Depends(get_db)):
+    return crud.create_health_record(db=db, health=health)
 
-@router.get("/health_records/", response_model=List[HealthRecord])
-def read_records(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return crud_health.get_health_records(db, skip=skip, limit=limit)
+@router.get("/{health_id}", response_model=schemas.Health)
+def read_health_record(health_id: str, db: Session = Depends(get_db)):
+    db_health = crud.get_health_record(db, health_id=health_id)
+    if not db_health:
+        raise HTTPException(status_code=404, detail="Health record not found")
+    return db_health
+
+@router.put("/{health_id}", response_model=schemas.Health)
+def update_health_record(health_id: str, health: schemas.HealthUpdate, db: Session = Depends(get_db)):
+    return crud.update_health_record(db, health_id, health)
+
+@router.delete("/{health_id}")
+def delete_health_record(health_id: str, db: Session = Depends(get_db)):
+    crud.delete_health_record(db, health_id)
+    return {"ok": True}
