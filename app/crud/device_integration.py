18635433b09ba9dc22118813ab5bcc305d8bb564
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-05 17:59:09 +1000

    Update device_integration.py

--- app/crud/device_integration.py
+++ app/crud/device_integration.py
@@ -1,11 +1,32 @@
-from app.models import DeviceIntegration
-from app.db.session import db
+from sqlalchemy.orm import Session
+from models import device_integration as models
+from schemas import device_integration as schemas
 
-def create_device_integration(device_integration):
-    db.add(device_integration)
+def create_device(db: Session, device: schemas.DeviceIntegrationCreate):
+    db_device = models.DeviceIntegration(**device.dict())
+    db.add(db_device)
     db.commit()
-    db.refresh(device_integration)
-    return device_integration
+    db.refresh(db_device)
+    return db_device
 
-def get_device_integration_by_id(device_integration_id):
-    return db.query(DeviceIntegration).filter(DeviceIntegration.id == device_integration_id).first()
+def get_devices(db: Session, skip=0, limit=100):
+    return db.query(models.DeviceIntegration).offset(skip).limit(limit).all()
+
+def get_device(db: Session, device_id: str):
+    return db.query(models.DeviceIntegration).filter(models.DeviceIntegration.id == device_id).first()
+
+def update_device(db: Session, device_id: str, updates: schemas.DeviceIntegrationUpdate):
+    db_device = db.query(models.DeviceIntegration).filter(models.DeviceIntegration.id == device_id).first()
+    if db_device:
+        for field, value in updates.dict(exclude_unset=True).items():
+            setattr(db_device, field, value)
+        db.commit()
+        db.refresh(db_device)
+    return db_device
+
+def delete_device(db: Session, device_id: str):
+    db_device = db.query(models.DeviceIntegration).filter(models.DeviceIntegration.id == device_id).first()
+    if db_device:
+        db.delete(db_device)
+        db.commit()
+    return db_device
