b7cbedfce5804c1df3cc18b18f4575f3e1227a3c
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-05 17:58:07 +1000

    Update reminder_log.py

--- app/crud/reminder_log.py
+++ app/crud/reminder_log.py
@@ -1,11 +1,33 @@
-from app.models import ReminderLog
-from app.db.session import db
+from sqlalchemy.orm import Session
+from models import reminder_log as models
+from schemas import reminder_log as schemas
 
-def create_reminder_log(reminder):
-    db.add(reminder)
+def create_reminder(db: Session, reminder: schemas.ReminderLogCreate):
+    db_reminder = models.ReminderLog(**reminder.dict())
+    db.add(db_reminder)
     db.commit()
-    db.refresh(reminder)
-    return reminder
+    db.refresh(db_reminder)
+    return db_reminder
 
-def get_reminder_log_by_id(reminder_id):
-    return db.query(ReminderLog).filter(ReminderLog.id == reminder_id).first()
+def get_reminders(db: Session, skip=0, limit=100):
+    return db.query(models.ReminderLog).offset(skip).limit(limit).all()
+
+def get_reminder(db: Session, reminder_id: str):
+    return db.query(models.ReminderLog).filter(models.ReminderLog.id == reminder_id).first()
+
+def update_reminder(db: Session, reminder_id: str, updates: schemas.ReminderLogUpdate):
+    db_reminder = db.query(models.ReminderLog).filter(models.ReminderLog.id == reminder_id).first()
+    if db_reminder:
+        for field, value in updates.dict(exclude_unset=True).items():
+            setattr(db_reminder, field, value)
+        db.commit()
+        db.refresh(db_reminder)
+    return db_reminder
+
+def delete_reminder(db: Session, reminder_id: str):
+    db_reminder = db.query(models.ReminderLog).filter(models.ReminderLog.id == reminder_id).first()
+    if db_reminder:
+        db.delete(db_reminder)
+        db.commit()
+    return db_reminder
+
