482f988d01f72609fb721fa5a13db1ed7d87d704
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-05 17:49:18 +1000

    Update medication.py

--- app/api/endpoints/medication.py
+++ app/api/endpoints/medication.py
@@ -1,9 +1,26 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app import crud, schemas
+from app.db.database import get_db
+
 router = APIRouter()
 
-@router.get("/", response_model=List[str])
-def read_medications(db: Session = Depends(get_db)):
-    return ["Sample medication"]
+@router.post("/", response_model=schemas.Medication)
+def create_medication(med: schemas.MedicationCreate, db: Session = Depends(get_db)):
+    return crud.create_medication(db=db, med=med)
 
-@router.post("/", response_model=str)
-def create_medication(db: Session = Depends(get_db)):
-    return "Created medication"
+@router.get("/{med_id}", response_model=schemas.Medication)
+def read_medication(med_id: str, db: Session = Depends(get_db)):
+    db_med = crud.get_medication(db, med_id=med_id)
+    if not db_med:
+        raise HTTPException(status_code=404, detail="Medication not found")
+    return db_med
+
+@router.put("/{med_id}", response_model=schemas.Medication)
+def update_medication(med_id: str, med: schemas.MedicationUpdate, db: Session = Depends(get_db)):
+    return crud.update_medication(db, med_id, med)
+
+@router.delete("/{med_id}")
+def delete_medication(med_id: str, db: Session = Depends(get_db)):
+    crud.delete_medication(db, med_id)
+    return {"ok": True}
