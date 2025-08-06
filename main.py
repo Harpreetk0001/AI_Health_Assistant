d21924d159c11e5dc160a200bfa2702aa10c0279
gurleen-dev <gurleensaini2019@gmail.com>
2025-08-06 20:40:45 +1000

    main.py

--- main.py
+++ main.py
@@ -2,6 +2,13 @@
 
 #import files for front-end, backend, dev, ai models
 
+from fastapi import FastAPI
+from app.api import fall_event
+
+app = FastAPI()
+
+app.include_router(fall_event.router, prefix="/fall_event", tags=["Fall Events"])
+
 #load files and data using APIs
 
 #define variables with data
