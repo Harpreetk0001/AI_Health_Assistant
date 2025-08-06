f2e48bfeb63891c0825426edd128f54933cc489e
mehb0-0 <mehakb.2318@gmail.com>
2025-08-06 14:40:11 +1000

    Create ToDoList.py

---
+++ ToDoList.py
@@ -0,0 +1,11 @@
+import json
+import numpy as np
+import time
+import matplotlib.pyplot as plt
+
+#to-do list items
+todoList = []
+
+#to do list class
+class ToDoList:
+  def __init__(self, self, timestamp = None):
