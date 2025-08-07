a42eb5fec8d20228ec348304ea87b0f5beab2030
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-07 21:52:21 +1000

    Update activity_log.py

--- app/crud/activity_log.py
+++ app/crud/activity_log.py
@@ -1,22 +1,22 @@
 from sqlalchemy.orm import Session
-from models import activity as models
-from schemas import activity as schemas
+from app.models.activity_log import ActivityLog as ModelActivityLog
+from app.schemas.activity_log import ActivityLogCreate, ActivityLogUpdate
 
-def create_activity(db: Session, activity: schemas.ActivityCreate):
-    db_activity = models.Activity(**activity.dict())
+def create_activity(db: Session, activity: ActivityLogCreate):
+    db_activity = ModelActivityLog(**activity.dict())
     db.add(db_activity)
     db.commit()
     db.refresh(db_activity)
     return db_activity
 
 def get_activities(db: Session, skip=0, limit=100):
-    return db.query(models.Activity).offset(skip).limit(limit).all()
+    return db.query(ModelActivityLog).offset(skip).limit(limit).all()
 
 def get_activity(db: Session, activity_id: str):
-    return db.query(models.Activity).filter(models.Activity.id == activity_id).first()
+    return db.query(ModelActivityLog).filter(ModelActivityLog.id == activity_id).first()
 
-def update_activity(db: Session, activity_id: str, updates: schemas.ActivityUpdate):
-    db_activity = db.query(models.Activity).filter(models.Activity.id == activity_id).first()
+def update_activity(db: Session, activity_id: str, updates: ActivityLogUpdate):
+    db_activity = db.query(ModelActivityLog).filter(ModelActivityLog.id == activity_id).first()
     if db_activity:
         for field, value in updates.dict(exclude_unset=True).items():
             setattr(db_activity, field, value)
@@ -25,7 +25,7 @@ def update_activity(db: Session, activity_id: str, updates: schemas.ActivityUpda
     return db_activity
 
 def delete_activity(db: Session, activity_id: str):
-    db_activity = db.query(models.Activity).filter(models.Activity.id == activity_id).first()
+    db_activity = db.query(ModelActivityLog).filter(ModelActivityLog.id == activity_id).first()
     if db_activity:
         db.delete(db_activity)
         db.commit()
