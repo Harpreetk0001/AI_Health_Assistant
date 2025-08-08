3da063d4a5073e918c6e3be0f0cecb59e8134662
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 21:07:43 +1000

    Update constants.py

--- app/core/constants.py
+++ app/core/constants.py
@@ -1,5 +1,5 @@
-from enum import Enum
-class UserRole(str, Enum):
+from enum import Enum # Import Enum class to create a list of named constant values
+class UserRole(str, Enum): # Define user roles in the system as a set of fixed options
     ELDERLY = "elderly"
     CAREGIVER = "caregiver"
     ADMIN = "admin"
