58d2f714da58f96f75b05686553f860899127f0e
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 21:11:00 +1000

    Update logging.py

--- app/core/logging.py
+++ app/core/logging.py
@@ -3,10 +3,10 @@ import sys          # Import sys module to access system-specific parameters and
 def setup_logging():
 # Set up basic configuration for logging
     logging.basicConfig(
-        level=logging.INFO,
-        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+        level=logging.INFO,    # Show all messages with level INFO and above (like warnings and errors)
+        format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",  # Define how log messages will look (time, level, source, message)
         handlers=[
-            logging.StreamHandler(sys.stdout)
+            logging.StreamHandler(sys.stdout)  # Send the log output to the console (standard output)
         ]
     )
 logger = logging.getLogger("Medbuddy") # Create a logger instance named "Medbuddy" which we will use throughout the app to write logs
