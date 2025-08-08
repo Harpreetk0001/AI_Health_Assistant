f6c8104ee1c3dc0ad3bbd8483cc94574a44a1303
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 21:08:59 +1000

    Update expectations.py

--- app/core/expectations.py
+++ app/core/expectations.py
@@ -1,10 +1,10 @@
-from fastapi import HTTPException, status
-class NotFoundException(HTTPException):
+from fastapi import HTTPException, status # Import HTTPException and status codes from FastAPI
+class NotFoundException(HTTPException): # Define a custom error for "Not Found" situations
     def __init__(self, detail: str = "Resource not found"):
         super().__init__(status_code=status.HTTP_404_NOT_FOUND, detail=detail)
-class UnauthorizedException(HTTPException):
+class UnauthorizedException(HTTPException): # Define a custom error for "Unauthorized" access
     def __init__(self, detail: str = "Unauthorized"):
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, detail=detail)
-class BadRequestException(HTTPException):
+class BadRequestException(HTTPException): # Define a custom error for "Bad Request" errors
     def __init__(self, detail: str = "Bad request"):
         super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
