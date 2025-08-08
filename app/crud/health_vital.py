64cde0311f03076933df7e98bf490cc956af9303
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 11:54:47 +1000

    Update health_vital.py

--- app/crud/health_vital.py
+++ app/crud/health_vital.py
@@ -1,28 +1,27 @@
 from sqlalchemy.orm import Session
-from models import health_vital as models
-from schemas import health_vital as schemas
+from app.models import health_vital
+from app.schemas import health_vital as schemas
 import uuid
-
 def create_health_vital(db: Session, hv: schemas.HealthVitalCreate):
-    db_hv = models.HealthVital(id=str(uuid.uuid4()), **hv.dict())
+    db_hv = health_vital.HealthVital(
+        id=str(uuid.uuid4()),
+        **hv.model_dump()
+    )
     db.add(db_hv)
     db.commit()
     db.refresh(db_hv)
     return db_hv
-
 def get_health_vitals(db: Session, user_id: str):
-    return db.query(models.HealthVital).filter(models.HealthVital.user_id == user_id).all()
-
+    return db.query(health_vital.HealthVital).filter(health_vital.HealthVital.user_id == user_id).all()
 def get_health_vital(db: Session, hv_id: str):
-    return db.query(models.HealthVital).filter(models.HealthVital.id == hv_id).first()
-
-def update_health_vital(db: Session, db_hv: models.HealthVital, hv: schemas.HealthVitalUpdate):
-    for key, value in hv.dict(exclude_unset=True).items():
+    return db.query(health_vital.HealthVital).filter(health_vital.HealthVital.id == hv_id).first()
+def update_health_vital(db: Session, db_hv: health_vital.HealthVital, hv: schemas.HealthVitalUpdate):
+    updates = hv.model_dump(exclude_unset=True)
+    for key, value in updates.items():
         setattr(db_hv, key, value)
     db.commit()
     db.refresh(db_hv)
     return db_hv
-
-def delete_health_vital(db: Session, db_hv: models.HealthVital):
+def delete_health_vital(db: Session, db_hv: health_vital.HealthVital):
     db.delete(db_hv)
     db.commit()
