3c8c6b36b99edf317e432ce2ef977acd6f7f0971
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 12:13:35 +1000

    Update suggestion.py

--- app/schemas/suggestion.py
+++ app/schemas/suggestion.py
@@ -1,20 +1,15 @@
 from pydantic import BaseModel
 from uuid import UUID
 from datetime import datetime
-
 class SuggestionBase(BaseModel):
     user_id: UUID
     suggestion_text: str
-
 class SuggestionCreate(SuggestionBase):
     pass
-
 class SuggestionUpdate(BaseModel):
     suggestion_text: str
-
 class SuggestionResponse(SuggestionBase):
     id: UUID
     created_at: datetime
-
     class Config:
         orm_mode = True
