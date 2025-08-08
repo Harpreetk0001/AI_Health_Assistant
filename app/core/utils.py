62bea6689d20cc54044952919c8d296cf6341095
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 21:14:15 +1000

    Update utils.py

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,3 +1,4 @@
-import uuid
+import uuid # Import the module to work with unique identifiers (UUIDs)
 def generate_uuid() -> str:
+    # Create a new unique ID and convert it to a string
     return str(uuid.uuid4())
