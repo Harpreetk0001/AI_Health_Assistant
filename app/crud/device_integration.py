639528335feb3e54ee72ad0d56943d0544fe92ed
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 11:52:48 +1000

    Update device_integration.py

--- app/crud/device_integration.py
+++ app/crud/device_integration.py
@@ -1,31 +1,26 @@
 from sqlalchemy.orm import Session
-from models import device_integration as models
-from schemas import device_integration as schemas
-
-def create_device(db: Session, device: schemas.DeviceIntegrationCreate):
-    db_device = models.DeviceIntegration(**device.dict())
+from app.models.device_integration import DeviceIntegration
+from app.schemas.device_integration import DeviceIntegrationCreate, DeviceIntegrationUpdate
+def create_device(db: Session, device: DeviceIntegrationCreate):
+    db_device = DeviceIntegration(**device.model_dump())
     db.add(db_device)
     db.commit()
     db.refresh(db_device)
     return db_device
-
-def get_devices(db: Session, skip=0, limit=100):
-    return db.query(models.DeviceIntegration).offset(skip).limit(limit).all()
-
+def get_devices(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(DeviceIntegration).offset(skip).limit(limit).all()
 def get_device(db: Session, device_id: str):
-    return db.query(models.DeviceIntegration).filter(models.DeviceIntegration.id == device_id).first()
-
-def update_device(db: Session, device_id: str, updates: schemas.DeviceIntegrationUpdate):
-    db_device = db.query(models.DeviceIntegration).filter(models.DeviceIntegration.id == device_id).first()
+    return db.query(DeviceIntegration).filter(DeviceIntegration.id == device_id).first()
+def update_device(db: Session, device_id: str, updates: DeviceIntegrationUpdate):
+    db_device = db.query(DeviceIntegration).filter(DeviceIntegration.id == device_id).first()
     if db_device:
-        for field, value in updates.dict(exclude_unset=True).items():
+        for field, value in updates.model_dump(exclude_unset=True).items():
             setattr(db_device, field, value)
         db.commit()
         db.refresh(db_device)
     return db_device
-
 def delete_device(db: Session, device_id: str):
-    db_device = db.query(models.DeviceIntegration).filter(models.DeviceIntegration.id == device_id).first()
+    db_device = db.query(DeviceIntegration).filter(DeviceIntegration.id == device_id).first()
     if db_device:
         db.delete(db_device)
         db.commit()
