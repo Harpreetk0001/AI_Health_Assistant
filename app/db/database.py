4eb841eae5843ec912c2ad2bdac173c5588d4b76
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 21:19:43 +1000

    Update database.py

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,8 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine # Import create_engine to connect to the database
+from sqlalchemy.ext.declarative import declarative_base # Import base class for our models
 from sqlalchemy.orm import sessionmaker
 import os
+# Get the database URL from environment variable, or use default if not set
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://postgres:Secret#123@localhost:5432/health_assistant")
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
