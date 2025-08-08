196bf1980242e92abe7191a744597c196eaa9d1a
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 11:51:32 +1000

    Update __init__.py

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -5,8 +5,24 @@ from . import emergency_contact
 from . import health_vital
 from . import fall_event
 from . import mental_health_log
-from . import reminder_log
+from . reminder_log import ReminderLogCreate, ReminderLogUpdate
 from . import conversation_log
 from . import device_integration
 from . import suggestion
 from . import ui_preference
+__all__ = [
+    "activity_log",
+    "user",
+    "medication",
+    "emergency_contact",
+    "health_vital",
+    "fall_event",
+    "mental_health_log",
+    "reminder_log",
+    "ReminderLogCreate",
+    "ReminderLogUpdate",
+    "conversation_log",
+    "device_integration",
+    "suggestion",
+    "ui_preference",
+]
