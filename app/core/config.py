ffec8aa6fb6a2bda6e13d4d64acc40ab10abed5f
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-08-08 14:03:25 +1000

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,9 @@
 from pydantic import BaseSettings
+__all__ = ["Settings", "settings"]
 class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # count as 1 day
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24  # 1 day
     DEBUG: bool = False
     class Config:
         env_file = ".env"
