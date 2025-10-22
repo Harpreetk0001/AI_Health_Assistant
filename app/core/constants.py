c8135db959ca06345fbd7fc80bb737d5ee68bd21
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-10-22 20:41:20 +1100

    Delete app/core directory

--- app/core/constants.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum # Import Enum class to create a list of named constant values
-class UserRole(str, Enum): # Define user roles in the system as a set of fixed options
-    ELDERLY = "elderly"
-    CAREGIVER = "caregiver"
-    ADMIN = "admin"
--- app/core/utils.py
+++
@@ -1,4 +0,0 @@
-import uuid # Import the module to work with unique identifiers (UUIDs)
-def generate_uuid() -> str:
-    # Create a new unique ID and convert it to a string
-    return str(uuid.uuid4())
