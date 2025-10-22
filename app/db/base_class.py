b02b1ef1d42521d0305cdb4ed73afdec27a8b772
Harpreetk0001 <kaur.harpreet01@yahoo.com>
2025-10-22 20:41:40 +1100

    Delete app/db directory

--- app/db/base_class.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.declarative import as_declarative, declared_attr # Import decorators to help define base class for SQLAlchemy models
-@as_declarative() # Mark this class as the base for all database models
-class Base:
-    id: any # Every model will have an 'id' attribute (type can be anything)
-    __name__: str # The name of the class as a string
-    @declared_attr  # This special method runs once per model class to set the table name
-    def __tablename__(cls) -> str:
-        # This will set the table name automatically as the lower-case class name
-        return cls.__name__.lower() # Automatically make the database table name the lowercase of the model class name
